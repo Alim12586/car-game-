9ecc9dbfb63bcea0b8e852fae594326075c4f3c0
Alim12586 <alimtas12@gmail.com>
2025-09-05 15:21:48 +0300

    Add files via upload

---
+++ code0.js
@@ -0,0 +1,20 @@
+gdjs.Game_32SceneCode = {};
+gdjs.Game_32SceneCode.localVariables = [];
+
+
+gdjs.Game_32SceneCode.eventsList0 = function(runtimeScene) {
+
+};
+
+gdjs.Game_32SceneCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+
+gdjs.Game_32SceneCode.eventsList0(runtimeScene);
+
+
+return;
+
+}
+
+gdjs['Game_32SceneCode'] = gdjs.Game_32SceneCode;
